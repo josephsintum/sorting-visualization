64b26d6df28be4adc8a9586360bba55c3d78ed56
josephsintum <sjumbam15@gmail.com>
2020-11-13 23:11:02 -0800

    adding material ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,37 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../src/theme';
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
 
-export default function App({ Component, pageProps }) {
   return (
-    <>
-      <GlobalStyle />
+    <React.Fragment>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
       <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
-    </>
-  )
+    </React.Fragment>
+  );
 }
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
